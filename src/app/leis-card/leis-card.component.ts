4fb74a6660eb167d32209b92ba5ce7baea1960ab
Gustavo Arendt <gustavo.arendt@outlook.com>
2021-06-19 15:31:07 -0300

    Abstração instituições e leis em componentes card

---
+++ src/app/leis-card/leis-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { LeisMulherModel } from '../services/leis-mulher-model';
+
+@Component({
+  selector: 'app-leis-card',
+  templateUrl: './leis-card.component.html',
+  styleUrls: ['./leis-card.component.css']
+})
+export class LeisCardComponent implements OnInit {
+
+  @Input() lei: LeisMulherModel | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
