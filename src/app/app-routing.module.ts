287c40caa046887f706328114aef722c3a7b508c
Lsamsilva <leila.samsilva@gmail.com>
2021-06-19 11:51:48 -0300

    criação menuTemplate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { MenuTemplateComponent } from './menu-template/menu-template.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component: MenuTemplateComponent,
+  children:[
+    {
+      path:'',
+      component: HomePageComponent
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
